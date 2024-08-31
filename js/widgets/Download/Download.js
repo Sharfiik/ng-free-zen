50ef57eec094927948d4ec47ee3038d71490531c
Sarfiik <oniks.mt@mail.ru>
2024-08-31 20:38:24 +0300

    Refactored Download.js

--- js/widgets/Download/Download.js
+++ js/widgets/Download/Download.js
@@ -1,26 +1,10 @@
+import { Title } from '../../ui/Title/Title.js';
+import { Link } from '../../ui/Link/Link.js';
+import { Image } from '../../ui/Images/Image.js';
+
 /**
  * @typedef {import('./types.ts').DownloadData} DownloadData
- * @typedef {import('./types.ts').TitleData} TitleData
  */
-
-/**
- * @function Title
- * @param {TitleData} data
- * @param {string} className
- * @returns {string} HTML
- */
-
-const Title = (data, className) => {
-  switch (data.priority) {
-    case 6: return `<h6 class="${className}">${data.content}</h6>`;
-    case 5: return `<h5 class="${className}">${data.content}</h5>`;
-    case 4: return `<h4 class="${className}">${data.content}</h4>`;
-    case 3: return `<h3 class="${className}">${data.content}</h3>`;
-    case 2: return `<h2 class="${className}">${data.content}</h2>`;
-    default: return `<h1 class="${className}">${data.content}</h1>`;
-  }
-};
-
 
 /**
  * @function DownloadData
@@ -35,11 +19,15 @@ export const Download = (data) => {
     <section class="download" id="download">
       <div class="download__wrapper">
         <div class="download__text-block">
-          ${Title(title, 'download__title')}
-          ${texts.map((text) => `<p class="download__copy">${text}</p>`).join('')}
-          <div class="download__links">Links</div>
+            ${Title(title, 'download__title')}
+            ${texts.map((text) => `<p class="download__copy">${text}</p>`).join('')}
+          <div class="download__links">
+            ${links.map((link) => Link(link, 'download__link')).join('')}
+          </div>
         </div>
-        <div class="download__image-block">Image</div>
+        <div class="download__image-block">
+          ${Image(image)}
+        </div>
       </div>
     </section>
   `;
