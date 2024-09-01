d55b2ac7a64eb7f9e8fba523c90465ce11bf93d4
Sarfiik <oniks.mt@mail.ru>
2024-09-01 11:28:09 +0300

    Added Warranty.js

---
+++ js/widgets/warranty/Warranty.js
@@ -0,0 +1,30 @@
+import { Title } from '../../ui/Title/Title.js';
+import { Image } from '../../ui/Images/Image.js';
+
+/**
+ * @typedef {import('./types.ts').WarrantyData} WarrantyData
+ */
+
+/**
+ * @function WarrantyData
+ * @param {WarrantyData} data
+ * @returns {string} HTML
+ */
+
+export const Warranty = (data) => {
+  const {title, texts, image} = data;
+
+  return `
+    <section class="warranty" id="warranty">
+      <div class="warranty__wrapper">
+        <div class="warranty__text-block">
+          ${Title(title, 'warranty__title')}
+          ${texts.map((text) => `<p class="warranty__copy">${text}</p>`).join('')}
+        </div>
+        <div class="warranty__image-block">
+        ${Image(image)}
+        </div>
+      </div>
+    </section>
+  `;
+};
