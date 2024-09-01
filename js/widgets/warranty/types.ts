6c4f634f9ede1bb9701e8e99760c93c42951da14
Sarfiik <oniks.mt@mail.ru>
2024-09-01 11:27:44 +0300

    Added types.ts

---
+++ js/widgets/warranty/types.ts
@@ -0,0 +1,8 @@
+import { TitleData } from "../../ui/Title/Title";
+import { ImageData } from "../../ui/Images/types"
+
+export type WarrantyData = {
+  title: TitleData;
+  texts: string[];
+  image: ImageData;
+};
