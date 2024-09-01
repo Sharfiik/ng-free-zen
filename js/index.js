aa88c53f867d803056a2c2994f2fdaa379bb8200
Sarfiik <oniks.mt@mail.ru>
2024-09-01 13:43:29 +0300

    Refactored index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ import { data } from "./data.js";
 import { Download } from "./widgets/Download/Download.js";
 import { Warranty } from "./widgets/Warranty/Warranty.js";
 import { Care } from "./widgets/Care/Care.js";
+import { Cashback } from "./widgets/Cashback/Cashback.js";
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 $root?.insertAdjacentHTML('beforeend', Care(data.care));
+$root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
