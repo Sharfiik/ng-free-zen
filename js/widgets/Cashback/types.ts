10930974c53d23aca99d175a136c838b3e90b3d7
Sarfiik <oniks.mt@mail.ru>
2024-09-01 13:43:43 +0300

    Added types.ts

---
+++ js/widgets/Cashback/types.ts
@@ -0,0 +1,8 @@
+import { TitleData } from '../../ui/Title/Title';
+import { ButtonData } from '../../ui/Button/types';
+
+export type CashbackData = {
+  title: TitleData;
+  texts: string[];
+  buttonText: ButtonData;
+};
