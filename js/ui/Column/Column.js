109b8c91bc113c01e3c471e28029ca90d1e3973d
Sarfiik <oniks.mt@mail.ru>
2024-09-04 20:14:49 +0300

    Added Column.js

---
+++ js/ui/Column/Column.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef {import('./types.ts').CategoryData} CategoryData
+ */
+
+/**
+ * @function Column
+ * @param {CategoryData} data
+ * @returns {string} HTML
+ */
+
+export const Column = (data) => {
+  const { links } = data;
+  return `
+  <div class="column">
+    <h3 class="column__title">${data.name}</h3>
+    <ul class="column__list">
+      ${links.map((link) => `
+        <li class="column__item">
+          <a href="${link.url}">
+            ${link.content.type === 'image' ?
+            `<img
+              src="${link.content.data}"
+              alt="${data.name}"
+            />
+            `: link.content.data}
+          </a></li>`).join('')}
+    </ul>
+  </div>
+  `;
+};
