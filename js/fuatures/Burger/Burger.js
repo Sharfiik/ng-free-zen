4b78d9c791245f614d2dde609b7a6a425a4fbd1a
Sarfiik <oniks.mt@mail.ru>
2024-09-07 21:23:11 +0300

    Added Burger.js

---
+++ js/fuatures/Burger/Burger.js
@@ -0,0 +1,11 @@
+/**
+ * @function Burger
+ * @param {string} className
+ * @returns {string} HTML
+ */
+
+export const Burger = (className) => `
+  <button class="${className}" type="button" id="burger">
+    <span></span>
+  </button>
+`;
