78794f8902aaa889bf904cf32c9925f7be5f8290
Sarfiik <oniks.mt@mail.ru>
2024-09-07 21:25:07 +0300

    Refsrtored Header.js

--- js/widgets/Header/Header.js
+++ js/widgets/Header/Header.js
@@ -1,5 +1,8 @@
 import { Logo } from '../../fuatures/Logo/Logo.js';
 import { Navigation } from '../../fuatures/Navigation/Navigation.js';
+import { Lang } from '../../fuatures/Lang/index.js';
+import { Theme } from '../../fuatures/Theme/index.js';
+import { Burger } from '../../fuatures/Burger/Burger.js';
 
 /**
  * @typedef {import('./types.ts').HeaderData} HeaderData
@@ -18,26 +21,10 @@ export const Header = (data) => {
       <div class="header__wrapper">
         ${Logo('header__logo')}
         ${Navigation(data.navigation)}
-
         <div class="header__actions">
-          <!-- Lang -->
-          <label class="header__lang">
-            <svg viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4.5 6L0 1.49973L1.50071 0L4.5 3.00053L7.49929 0L9 1.49973L4.5 6Z"/>
-            </svg>
-            <select id="lang">
-              <option value="en">EN</option>
-              <option value="ru">RU</option>
-            </select>
-          </label>
-            <!-- Theme -->
-          <button class="header__theme" type="button" id="theme" data-theme="light">
-            <img src="assets/icons/moon.svg" alt="Moon" />
-          </button>
-            <!-- Burger -->
-          <button class="burger" type="button" id="burger">
-            <span></span>
-          </button>
+          ${Lang(data.langs)}
+          ${Theme('header__theme')}
+          ${Burger('burger')}
         </div>
       </div>
     </header>
