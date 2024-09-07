13cb4fff2b7de81840643aad11bd57631074be31
Sarfiik <oniks.mt@mail.ru>
2024-09-07 21:24:26 +0300

    Added Theme.js

---
+++ js/fuatures/Theme/Theme.js
@@ -0,0 +1,11 @@
+/**
+ * @function Theme
+ * @param {string} className
+ * @returns {string} HTML
+ */
+
+export const Theme = (className) => `
+  <button class="${className}" type="button" id="theme" data-theme="light">
+    <img src="assets/icons/moon.svg" alt="Moon" />
+  </button>
+`;
