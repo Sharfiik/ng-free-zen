a5c06a20c487952ea9b5c6eb214c59cc5250c6f3
Sarfiik <oniks.mt@mail.ru>
2024-09-08 15:44:03 +0300

    Refactored index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 import { data } from './data.js';
+import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
 import { Warranty } from './widgets/Warranty/Warranty.js';
 import { Care } from './widgets/Care/Care.js';
 import { Cashback } from './widgets/Cashback/Cashback.js';
 import { Clients } from './widgets/Сlients/Сlients.js';
 import { Footer } from './widgets/Footer/Footer.js';
-import { Header } from './widgets/Header/Header.js';
 
 const $root = document.querySelector('#root');
 
+$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 $root?.insertAdjacentHTML('beforeend', Care(data.care));
 $root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
 $root?.insertAdjacentHTML('beforeend', Clients(data.clients));
 $root?.insertAdjacentHTML('beforeend', Footer(data.secondaryInfo));
-$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
