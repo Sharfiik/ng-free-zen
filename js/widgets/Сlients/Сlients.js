8e33092fce506b342cc1986c06b0460c1a2a8fe7
Sarfiik <oniks.mt@mail.ru>
2024-09-08 16:37:57 +0300

    Refactored Clients.js

--- js/widgets/Сlients/Сlients.js
+++ js/widgets/Сlients/Сlients.js
@@ -17,8 +17,7 @@ export const Clients = (data) => {
         <ul class="clients__brands">
           ${brands.map((brand) => `
             <li class="clients__brand">
-            <img
-              src="${brand.logo.lightSource}"
+            <img src="${brand.logo.lightSource}"
               alt="${brand.name}"
             />
             </li>
